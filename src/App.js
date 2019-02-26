d39abde0a8262653f0f14dc9edc132da864a1ec4
Braden Zhou <braden.zhou@aliyun.com>
2019-02-26 18:38:59 +1300

    add landing image animation

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const drawerWidth = 240;
 
 const styles = theme => ({
   root: {
-    display: 'flex',
+    display: 'flex'
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
@@ -167,100 +167,7 @@ class MiniDrawer extends React.Component {
     const { classes, theme } = this.props;
     const { loggedIn } = this.state;
     return (
-      <div className={classes.root}>
-        <LandingContainer />
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: this.state.open,
-          })}
-        >
-          <Toolbar disableGutters={!this.state.open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, {
-                [classes.hide]: this.state.open,
-              })}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Higher Consciousness Network
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          className={classNames(classes.drawer, {
-            [classes.drawerOpen]: this.state.open,
-            [classes.drawerClose]: !this.state.open,
-          })}
-          classes={{
-            paper: classNames({
-              [classes.drawerOpen]: this.state.open,
-              [classes.drawerClose]: !this.state.open,
-            }),
-          }}
-          open={this.state.open}
-        >
-          <div className={classes.toolbar}>
-            <IconButton onClick={this.handleDrawerClose}>
-              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            {['Category 1', 'Category 2', 'Category 3', 'Category 4'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Typography variant="title">
-          Marijuana Lollipop May Have Triggered Man's Heart Attack
-          </Typography>
-          <Typography variant="subtitle2">
-          By Rachael Rettner, Senior Writer | February 11, 2019 06:55am ET
-          </Typography>
-          <Divider />
-          <Typography paragraph>
-          A marijuana lollipop with a very high dose of the drug's active ingredient tetrahydrocannabinol (THC) may have triggered a man's heart attack, according to a new report of the man's case.
-
-The 70-year-old man decided to try edible marijuana to see if it would reduce the pain from his osteoarthritis and help him sleep. The man had smoked some marijuana in his youth, but had never tried an edible product, according to the report, which was published today (Feb. 11) in the Canadian Journal of Cardiology.
-          </Typography>
-          {
-            loggedIn ?
-            <Typography>
-              One night, the man consumed nearly an entire marijuana lollipop, which contained a staggering 90 milligrams of THC, more than 12 times the dose in a typical joint.
-
-Within a half hour, the man experienced "fearful hallucinations," followed by "crushing chest pain," the report said.
-
-The man was taken to the hospital, where doctors determined that he had had a heart attack. The patient had a known history of heart disease, but he was taking several medications for his condition, and had not experienced a heart problem for more than two years. [25 Odd Facts About Marijuana]
-
-
-            </Typography>
-
-            :
-            <div className="fb-login-button" data-size="large" data-button-type="login_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
-          }
-        </main>
-      </div>
+      <LandingContainer />
     );
   }
 }
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,20 +1,66 @@
 import React from 'react';
-import intl from 'react-intl-universal'
-import style from './landing.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
 
-export default class Landing extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  componentWillMount() {
-  }
-  componentDidMount() {
-  }
+const styles = theme => ({
+  card: {
+    width: '100%',
+    height: '100%'
+  },
+  media: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: '0',
+    left: '0',
+    zIndex: '0',
+    animation: '7s ease-out 0s 1 scaleAnimation'
+  },
+  actions: {
+    display: 'block',
+    textAlign: "center",
+    position: 'fixed',
+    bottom: '0',
+    left: '0',
+    right: '0'
+  },
+  "@keyframes scaleAnimation": {
+    from: { transform: 'scale(1.2)' },
+    to: { transform: 'scale(1)' },
+  },
+});
+
+class Landing extends React.Component {
+  state = {};
+
+  handleScrollButtonClick = () => {
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className={style.landing}>
-        Welcome to HCN.
-      </div>
-    )
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image="/images/matt-hardy-562566-unsplash@2x.png"
+        />
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Share">
+            <img src='/images/scroll-icon-png_02-.png' width={24} height={32.6} alt='scroll' />
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
   }
 }
+
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Landing);
