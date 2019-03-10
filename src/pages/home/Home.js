ca45a1a3e098fb98d8eebbe8185a76703c033d31
Alex Fung <alex@starrian.com>
2019-03-11 11:54:21 +1300

    Add Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,26 +7,87 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import AnimatedText from '../../components/AnimatedText';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+import red from '@material-ui/core/colors/red';
 
 import {
   APP_TITLE,
   SECOND_GENOME
 } from '../../common/i18n';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffffff'
+    }
+  },
+});
+
 const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  textButton:{
+    fontFamily: 'Calibri',
+    '&:hover': {
+      backgroundColor:'#ffffff',
+      paddingtop:'10px'
+    },
+  }
 });
 
 class Home extends React.Component {
   state = {};
 
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
   render() {
     const { classes } = this.props;
     return (
-      <span>Home Page</span>
+      <div className={classes.root}>
+        <MuiThemeProvider theme={theme}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Button className={classes.textButton}>
+                INTRO
+              </Button>
+              <Button className={classes.textButton}>
+                CONTENT
+          </Button>
+              <Button>
+                SECOND HCN Genome
+          </Button>
+              <Button className={classes.textButton}>
+                ORGANIZE
+          </Button>
+              <Button className={classes.textButton}>
+                CONNECT
+          </Button>
+            </Toolbar>
+          </AppBar>
+        </MuiThemeProvider>
+      </div>
     );
   }
 }
