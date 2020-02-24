4f9f8744ec9ff1643a0bf69135e495e4913e0c78
James Li <pingzhouli@gmail.com>
2020-02-24 16:08:18 +1300

    seperate home page into home page and landing page

---
+++ src/pages/landing/LandingContainer.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux'
+
+import Landing from './Landing'
+
+const mapStateToProps = state => {
+  return {
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
