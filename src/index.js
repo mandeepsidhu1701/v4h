221443f00c575f437d7e12cdf1446377e894477d
Nhat-Hoai Ho <peterhoai0803@gmail.com>
2020-02-26 10:50:26 +1300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
-import { Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { ConnectedRouter, routerMiddleware } from 'connected-react-router';
+import {Route, Switch} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import {createStore, compose, applyMiddleware} from 'redux';
+import {ConnectedRouter, routerMiddleware} from 'connected-react-router';
 import intl from 'react-intl-universal';
 import rootReducer from './reducers';
 import Amplify from 'aws-amplify';
@@ -15,37 +15,32 @@ import './index.css';
 import HomeContainer from './pages/home/HomeContainer';
 import ArticlesContainer from './pages/articles/ArticlesContainer';
 
-import routes from "./data/routes";
+import routes from './data/routes';
 
-Amplify.configure(config)
+Amplify.configure(config);
 
-const history = createBrowserHistory()
+const history = createBrowserHistory();
 
 const store = createStore(
   rootReducer(history),
   {},
-  compose(
-    applyMiddleware(
-      thunkMiddleware,
-      routerMiddleware(history)
-    )
-  )
-)
+  compose(applyMiddleware(thunkMiddleware, routerMiddleware(history)))
+);
 
-window.intl = intl
-window.reduxStore = store
+window.intl = intl;
+window.reduxStore = store;
 
 const Routes = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div className="primary-layout">
         <main>
-          <Route path={routes.Home} exact component={HomeContainer} />      
+          <Route path={routes.Home} exact component={HomeContainer} />
           <Route path={routes.Articles} exact component={ArticlesContainer} />
         </main>
       </div>
     </ConnectedRouter>
   </Provider>
-)
+);
 
 render(<Routes />, document.getElementById('root'));
