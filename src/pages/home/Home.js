a6f1cdf45a495874f9ca7f2ab620e6e5732ff54c
Nhat-Hoai Ho <peterhoai0803@gmail.com>
2020-02-27 10:12:51 +1300

    fix code style

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,21 +3,14 @@ import {withRouter, Link as RouterLink} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {withStyles, IconButton, Modal, Grid, Button} from '@material-ui/core';
 import PlayCircleOutline from '@material-ui/icons/PlayCircleOutline';
-import {
-  Sphere,
-  AnimatedText,
-  BackVideoWithNavBarWrapper as PageWrapper
-} from '../../components/baseComponents';
+import {Sphere, AnimatedText, BackVideoWithNavBarWrapper as PageWrapper} from '../../components/baseComponents';
 import SignUpModal, {SIGN_UP} from '../../components/auth/signup';
 import SignInModal, {SIGN_IN} from '../../components/auth/signin';
 
 import routes from '../../data/routes';
 import {userRequestForgotPasswordSubmit} from '../../actions/auth';
 
-import {
-  Carousel,
-  CarouselSlide
-} from '../../components/home/CorePrinciplesCarousel';
+import {Carousel, CarouselSlide} from '../../components/home/corePrinciplesCarousel';
 
 import styles from './HomeStyles';
 
@@ -136,13 +129,7 @@ class Home extends React.Component {
   };
 
   render() {
-    const {
-      landing,
-      showModal,
-      authForm,
-      playingVolunteerVideo,
-      showAuthLinks
-    } = this.state;
+    const {landing, showModal, authForm, playingVolunteerVideo, showAuthLinks} = this.state;
     const {classes} = this.props;
 
     let landingRoot;
@@ -161,12 +148,7 @@ class Home extends React.Component {
 
     const slides = data.corePrincipals.map((principle, i) => {
       return (
-        <CarouselSlide
-          key={principle.name}
-          imageURL={principle.image}
-          title={principle.name}
-          text={principle.text}
-        />
+        <CarouselSlide key={principle.name} imageURL={principle.image} title={principle.name} text={principle.text} />
       );
     });
 
@@ -178,27 +160,13 @@ class Home extends React.Component {
             alt="Background aesthetics"
             className={`${classes.landingImage} ${classes.landingImageAnimation}`}
           />
-          <span
-            className={classes.genome}
-            onMouseEnter={this.handleShowAuthLinks}
-            onClick={this.handleShowAuthLinks}
-          >
+          <span className={classes.genome} onMouseEnter={this.handleShowAuthLinks} onClick={this.handleShowAuthLinks}>
             Second Genome
           </span>
-          <span
-            className={
-              showAuthLinks
-                ? `${classes.fontBase} ${classes.authSpan}`
-                : classes.hidden
-            }
-          >
+          <span className={showAuthLinks ? `${classes.fontBase} ${classes.authSpan}` : classes.hidden}>
             <RouterLink
               to="#"
-              className={
-                authForm === SIGN_UP
-                  ? `${classes.authLink} ${highlightedAuthLinkClass}`
-                  : classes.authLink
-              }
+              className={authForm === SIGN_UP ? `${classes.authLink} ${highlightedAuthLinkClass}` : classes.authLink}
               onMouseEnter={this.handleShowSignUpForm}
               onClick={this.handleShowSignUpForm}
             >
@@ -207,11 +175,7 @@ class Home extends React.Component {
             <span className={classes.authSpacer} />
             <RouterLink
               to="#"
-              className={
-                authForm === SIGN_IN
-                  ? `${classes.authLink} ${highlightedAuthLinkClass}`
-                  : classes.authLink
-              }
+              className={authForm === SIGN_IN ? `${classes.authLink} ${highlightedAuthLinkClass}` : classes.authLink}
               onMouseEnter={this.handleShowLoginForm}
               onClick={this.handleShowLoginForm}
             >
@@ -220,10 +184,7 @@ class Home extends React.Component {
           </span>
 
           <div className={classes.siteName}>
-            <AnimatedText
-              text={'Higher Consciousness Network'}
-              delays={appTitleDelays}
-            />
+            <AnimatedText text={'Higher Consciousness Network'} delays={appTitleDelays} />
           </div>
 
           <div className={classes.sphereSideBar}>
@@ -249,20 +210,11 @@ class Home extends React.Component {
                 this.handleScrollLanding();
               }}
             >
-              <img
-                src="/images/scroll-icon-png_02-.png"
-                width={24}
-                height={32}
-                alt="Close Landing Drawer"
-              />
+              <img src="/images/scroll-icon-png_02-.png" width={24} height={32} alt="Close Landing Drawer" />
             </IconButton>
           </div>
 
-          <Modal
-            open={showModal}
-            onClose={this.handleHideForms}
-            className={classes.modalOverflow}
-          >
+          <Modal open={showModal} onClose={this.handleHideForms} className={classes.modalOverflow}>
             {authForm === SIGN_UP ? (
               <SignUpModal handleCloseForm={this.handleHideForms} />
             ) : (
@@ -278,42 +230,29 @@ class Home extends React.Component {
             <Grid item xs={1} sm={2} md={3}></Grid>
             <Grid item xs={10} sm={8} md={6}>
               <p className={classes.inspireQuote}>
-                "A dream you dream alone is only a dream. A dream you dream
-                together is reality."
+                "A dream you dream alone is only a dream. A dream you dream together is reality."
               </p>
-              <p className={classes.inspireQuoteCitation}>
-                JOHN LENNON & YOKO ONO
-              </p>
+              <p className={classes.inspireQuoteCitation}>JOHN LENNON & YOKO ONO</p>
             </Grid>
             <Grid item xs={1} sm={2} md={3}></Grid>
           </Grid>
           <Grid container className={classes.gridContainerPadding}>
             <Grid item xs={12} sm={12} md={4}>
-              <p
-                className={`${classes.paragraphTextSmall} ${classes.siteDescriptionContainer}`}
-              >
-                HCN is a "Metamorphic Media" site aimed at elevating Global
-                Consciousness via valuable quotes, affirmations, articles,
-                videos and visualizations. Content is being generated to
-                integrate with the forth coming "Sanctuary Project" and the
-                services of human and global advancement architecture that the
-                project offers. Engage with HCN and holistically advance;
-                physically, mentally, emotionally and spiritually, to possess
-                greater awareness and a surplus capacity to effortlessly help
-                others and the progression of the planet. HCN is also a platform
-                that brings together a network of like minded Conscious
-                Creatures (conscious; consumers, producers, creators and
-                communicators) as a collective representation to transform our
-                world. Consume, Contribute, Disperse, and Sponsor media which
-                uplifts the human spirit, empowers people and nurtures the
-                planet.
+              <p className={`${classes.paragraphTextSmall} ${classes.siteDescriptionContainer}`}>
+                HCN is a "Metamorphic Media" site aimed at elevating Global Consciousness via valuable quotes,
+                affirmations, articles, videos and visualizations. Content is being generated to integrate with the
+                forth coming "Sanctuary Project" and the services of human and global advancement architecture that the
+                project offers. Engage with HCN and holistically advance; physically, mentally, emotionally and
+                spiritually, to possess greater awareness and a surplus capacity to effortlessly help others and the
+                progression of the planet. HCN is also a platform that brings together a network of like minded
+                Conscious Creatures (conscious; consumers, producers, creators and communicators) as a collective
+                representation to transform our world. Consume, Contribute, Disperse, and Sponsor media which uplifts
+                the human spirit, empowers people and nurtures the planet.
               </p>
               <p className={classes.paragraphTextSmall}>
-                Second Genome, and it's messenger system, represents how we as
-                humans and humanity have the ability to evolve beyond our
-                current state to collectively create Inter-Generational Global
-                Family Wealth. Connect as a Conscious Creature on this site and
-                learn more of what that means!{' '}
+                Second Genome, and it's messenger system, represents how we as humans and humanity have the ability to
+                evolve beyond our current state to collectively create Inter-Generational Global Family Wealth. Connect
+                as a Conscious Creature on this site and learn more of what that means!{' '}
                 <RouterLink to="#">Connect Me!</RouterLink>
               </p>
             </Grid>
@@ -337,57 +276,31 @@ class Home extends React.Component {
                   </Grid>
                 </Grid>
                 <Grid item xs={12} sm={12} md={5}>
-                  <Grid
-                    container
-                    direction="column"
-                    className={classes.aboutUsContainer}
-                  >
-                    <Grid
-                      item
-                      className={`${classes.fontBase} ${classes.aboutUsTitle}`}
-                    >
+                  <Grid container direction="column" className={classes.aboutUsContainer}>
+                    <Grid item className={`${classes.fontBase} ${classes.aboutUsTitle}`}>
                       ABOUT US
                     </Grid>
-                    <Grid
-                      item
-                      className={`${classes.fontBase} ${classes.aboutUsText}`}
-                    >
-                      HCN and the Sanctuary Project are the ofspring of Vision
-                      for Humanity, a New Zealand charitable trust with aims to
-                      create a Global Family and a Successful Planet. Long term,
-                      HCN is intended to disperse content from The Urban
-                      Sanctuary's health and wellbeing professionals, guides and
+                    <Grid item className={`${classes.fontBase} ${classes.aboutUsText}`}>
+                      HCN and the Sanctuary Project are the ofspring of Vision for Humanity, a New Zealand charitable
+                      trust with aims to create a Global Family and a Successful Planet. Long term, HCN is intended to
+                      disperse content from The Urban Sanctuary's health and wellbeing professionals, guides and
                       workshops.
                     </Grid>
                   </Grid>
                 </Grid>
                 <Grid item xs={12} sm={12} md={12}>
-                  <Grid
-                    container
-                    direction="column"
-                    className={classes.metamorphicMediaContainer}
-                  >
+                  <Grid container direction="column" className={classes.metamorphicMediaContainer}>
                     <Grid item>
-                      <div
-                        className={`${classes.fontBase} ${classes.metamorphicMediaTitle}`}
-                      >
-                        METAMORPHIC MEDIA
+                      <div className={`${classes.fontBase} ${classes.metamorphicMediaTitle}`}>METAMORPHIC MEDIA</div>
+                    </Grid>
+                    <Grid item>
+                      <div className={`${classes.fontBase} ${classes.metamorphicMediaText}`}>
+                        Elevating Global Consciousness through all forms of information and entertainment. Visualise a
+                        world where the media induces mental / emotional well-bring and social cohesion.
                       </div>
                     </Grid>
                     <Grid item>
-                      <div
-                        className={`${classes.fontBase} ${classes.metamorphicMediaText}`}
-                      >
-                        Elevating Global Consciousness through all forms of
-                        information and entertainment. Visualise a world where
-                        the media induces mental / emotional well-bring and
-                        social cohesion.
-                      </div>
-                    </Grid>
-                    <Grid item>
-                      <div
-                        className={`${classes.fontBase} ${classes.metamorphicMediaCaption}`}
-                      >
+                      <div className={`${classes.fontBase} ${classes.metamorphicMediaCaption}`}>
                         Vision for Media, vision for Humanity
                       </div>
                     </Grid>
@@ -408,37 +321,23 @@ class Home extends React.Component {
             <Grid item xs={12} sm={12} md={6}>
               <div className={classes.videoParentContainer}>
                 <div className={classes.videoContainer}>
-                  <span
-                    className={`${classes.fontBase} ${classes.videoContainerTitle}`}
-                  >
+                  <span className={`${classes.fontBase} ${classes.videoContainerTitle}`}>
                     OUR VOLUNTEER BASED HCN TEAM
                   </span>
-                  <video
-                    ref="vidRef"
-                    className={classes.videoVolunteers}
-                    controls={playingVolunteerVideo}
-                  >
+                  <video ref="vidRef" className={classes.videoVolunteers} controls={playingVolunteerVideo}>
                     <source src={volunteerVideoURL} type="video/mp4" />
                     Your browser does not support HTML5 video.
                   </video>
                   <IconButton
-                    className={
-                      playingVolunteerVideo
-                        ? classes.hidden
-                        : classes.videoVolunteerPlayButton
-                    }
+                    className={playingVolunteerVideo ? classes.hidden : classes.videoVolunteerPlayButton}
                     onClick={this.handleVolunteerVideoState}
                   >
-                    <PlayCircleOutline
-                      className={classes.videoVolunteerPlayIcon}
-                    />
+                    <PlayCircleOutline className={classes.videoVolunteerPlayIcon} />
                   </IconButton>
                 </div>
                 <div className={`${classes.fontBase} ${classes.videoCaption}`}>
-                  HCN is currently being established by a band of dedicated
-                  volunteers directed by the VFH founder Kim Larking. Watch this
-                  brief video to see out team (video of volunteers and me
-                  talking about HCN).
+                  HCN is currently being established by a band of dedicated volunteers directed by the VFH founder Kim
+                  Larking. Watch this brief video to see out team (video of volunteers and me talking about HCN).
                 </div>
               </div>
             </Grid>
@@ -450,11 +349,7 @@ class Home extends React.Component {
               </Carousel>
             </Grid>
             <Grid item xs={12} sm={12} md={2}>
-              <Grid
-                container
-                direction="column"
-                className={classes.urbanSanctuaryContainer}
-              >
+              <Grid container direction="column" className={classes.urbanSanctuaryContainer}>
                 <Grid item>
                   {/* TODO get correct image */}
                   <img
@@ -466,9 +361,7 @@ class Home extends React.Component {
                 <Grid item>
                   {/* TODO change "to" attrubute to correct link  */}
                   <RouterLink className={classes.linkBase} to="#">
-                    <Button
-                      className={`${classes.fontBase} ${classes.buttonBase} ${classes.urbanSanctuaryButton}`}
-                    >
+                    <Button className={`${classes.fontBase} ${classes.buttonBase} ${classes.urbanSanctuaryButton}`}>
                       VISIT NOW
                     </Button>
                   </RouterLink>
@@ -478,21 +371,12 @@ class Home extends React.Component {
             <Grid item md={1}></Grid>
           </Grid>
 
-          <Grid
-            container
-            className={`${classes.gridContainerPadding} ${classes.metawheelContainer}`}
-          >
+          <Grid container className={`${classes.gridContainerPadding} ${classes.metawheelContainer}`}>
             <Grid item sm={1} md={5}></Grid>
             <Grid item xs={12} sm={10} md={6}>
               {/* TODO get correct image */}
-              <img
-                className={classes.metawheelImage}
-                src="/images/home/square_wireframe.jpg"
-                alt="Metawheel Graphic"
-              />
-              <div className={`${classes.fontBase} ${classes.metawheelTitle}`}>
-                TAKE THE METAWHEEL JOURNEY
-              </div>
+              <img className={classes.metawheelImage} src="/images/home/square_wireframe.jpg" alt="Metawheel Graphic" />
+              <div className={`${classes.fontBase} ${classes.metawheelTitle}`}>TAKE THE METAWHEEL JOURNEY</div>
               <div className={`${classes.fontBase} ${classes.metawheelText}`}>
                 Sanctuary Human Advancement Architecture
               </div>
@@ -506,7 +390,7 @@ class Home extends React.Component {
 }
 
 Home.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withRouter(withStyles(styles)(Home));
