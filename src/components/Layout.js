78108e7cc56ec4d6b9156ddcce3691fda7bf2a10
Dreamufor <missmengxue@gmail.com>
2020-03-02 12:31:54 +1300

    add and fix footer at bottom

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {NavigationBar} from './baseComponents';
+import Footer from './baseComponents/Footer/Footer';
 
 export default function Layout(props) {
   return (
     <div>
-      <NavigationBar />
-      {props.children}
+      <main style={{minHeight: 'calc(100vh - 70px)'}}>
+        {/* when use calc, there should be space before and after operator, or it is invalid */}
+        <NavigationBar />
+        {props.children}
+      </main>
+      <Footer />
     </div>
   );
 }
