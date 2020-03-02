fcd5924780dfd8e65002844f1c4f01d9091e26c0
Dreamufor <missmengxue@gmail.com>
2020-03-02 15:03:43 +1300

    Change footer style and layout

--- src/components/baseComponents/Footer/Footer.js
+++ src/components/baseComponents/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {Typography, Grid, Container} from '@material-ui/core';
-import VerticalAlignTopIcon from '@material-ui/icons/VerticalAlignTop';
 import {withStyles} from '@material-ui/core/styles';
 import {Link} from 'react-router-dom';
 import './footer.css';
+import {contentSubmenu, connectSubmenu, organizeSubmenu, introSubmenu} from '../submenu/submenuData';
 
 const styles = (theme) => ({
   root: {
@@ -11,93 +11,89 @@ const styles = (theme) => ({
   }
 });
 
-function scrollToTop() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
-}
-
 class Footer extends Component {
   render() {
-    const {classes} = this.props;
     const preventDefault = (event) => event.preventDefault();
     return (
       <footer>
         <Container maxWidth="lg">
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
-            <Grid item>
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item md={4} lg={4} sm={2} xs={12}>
               <Typography className="HCN" variant="h5">
                 HCN
               </Typography>
             </Grid>
-            <Grid item>
-              <ul>
-                <li>
-                  <Link
-                    href="#"
-                    onClick={preventDefault}
-                    className="footer-link"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  {' '}
-                  <Link
-                    href="#"
-                    onClick={preventDefault}
-                    variant="body2"
-                    className="footer-link"
-                  >
-                    Intro
-                  </Link>
-                </li>
-                <li>
-                  {' '}
-                  <Link
-                    href="#"
-                    onClick={preventDefault}
-                    variant="body2"
-                    className="footer-link"
-                  >
-                    content
-                  </Link>
-                </li>
-                <li>
-                  {' '}
-                  <Link
-                    href="#"
-                    onClick={preventDefault}
-                    variant="body2"
-                    className="footer-link"
-                  >
-                    organize
-                  </Link>
-                </li>
-                <li>
-                  {' '}
-                  <Link
-                    href="#"
-                    onClick={preventDefault}
-                    variant="body2"
-                    className="footer-link"
-                  >
-                    connect
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="#"
-                    onClick="window.scrollTo({top: 0, behavior: 'smooth'})"
-                    className="footer-link"
-                  >
-                    Go to top
-                  </Link>
-                </li>
-              </ul>
+            <Grid item md={8} lg={8} sm={10} xs={12}>
+              <Grid container>
+                <Grid item xs={12} sm={6} md={3}>
+                  <ul>
+                    <li>
+                      {' '}
+                      <Link href="#" onClick={preventDefault} variant="body2" className="footer-title">
+                        INTRO
+                      </Link>
+                    </li>
+                    {introSubmenu.map((m, index) => (
+                      <li key={index}>
+                        <Link to={m.link} variant="body2" className="footer-link">
+                          {m.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                  <ul>
+                    <li>
+                      {' '}
+                      <Link href="#" onClick={preventDefault} variant="body2" className="footer-title">
+                        CONTENT
+                      </Link>
+                    </li>
+                    {contentSubmenu.map((m, index) => (
+                      <li key={index}>
+                        <Link to={m.link} variant="body2" className="footer-link">
+                          {m.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                  <ul>
+                    <li>
+                      {' '}
+                      <Link href="#" onClick={preventDefault} variant="body2" className="footer-title">
+                        ORGANIZE
+                      </Link>
+                    </li>
+                    {organizeSubmenu.map((m, index) => (
+                      <li key={index}>
+                        <Link to={m.link} variant="body2" className="footer-link">
+                          {m.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                  <ul>
+                    <li>
+                      {' '}
+                      <Link href="#" onClick={preventDefault} variant="body2" className="footer-title">
+                        CONNECT
+                      </Link>
+                    </li>
+                    {connectSubmenu.map((m, index) => (
+                      <li key={index}>
+                        <Link to={m.link} variant="body2" className="footer-link">
+                          {m.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </Container>
