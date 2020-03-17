8d567f61a10c8a5eb0168b47a026fb4a4554e183
James Li <pingzhouli@gmail.com>
2020-03-18 12:22:37 +1300

    extract words and paragraphs into data file

--- src/pages/home/Sections/SectionDescription.js
+++ src/pages/home/Sections/SectionDescription.js
@@ -6,8 +6,6 @@ import PlayCircleOutline from '@material-ui/icons/PlayCircleOutline';
 import {Carousel, CarouselSlide} from '../../../components/ui/Carousel';
 import ButterflyLarge from '../../../assets/images/icons/butterflyLarge.png';
 import UrbanSanctuary from '../../../assets/images/icons/urbanSanctuary.png';
-import Planting from '../../../assets/images/content/landing/planting.png';
-import Group from '../../../assets/images/content/landing/group.png';
 import Metawheel from '../../../assets/images/content/landing/metawheel.png';
 import data from '../../../data/homeData';
 import styles from '../HomeStyles';
@@ -30,32 +28,22 @@ function SectionDescription(props) {
   return (
     <div style={{background: 'url(/images/matt-hardy-562566-unsplash@2x.png)'}}>
       <Grid container className={classes.gridContainerPadding}>
-        <Grid item xs={1} sm={2} md={3} />
-        <Grid item xs={10} sm={8} md={6}>
+        <Grid item xs={1} sm={2}/>
+        <Grid item xs={10} sm={8}>
           <p className={classes.inspireQuote}>
-            "A dream you dream alone is only a dream. A dream you dream together is reality."
+            {data.text.inspireQuote}
           </p>
-          <p className={classes.inspireQuoteCitation}>JOHN LENNON & YOKO ONO</p>
+  <p className={classes.inspireQuoteCitation}>{data.text.inspireQuoteAuther}</p>
         </Grid>
         <Grid item xs={1} sm={2} md={3} />
       </Grid>
       <Grid container className={classes.gridContainerPadding}>
         <Grid item xs={12} sm={12} md={4}>
           <p className={`${classes.paragraphTextSmall} ${classes.siteDescriptionContainer}`}>
-            HCN is a "Metamorphic Media" site aimed at elevating Global Consciousness via valuable quotes, affirmations,
-            articles, videos and visualizations. Content is being generated to integrate with the forth coming
-            "Sanctuary Project" and the services of human and global advancement architecture that the project offers.
-            Engage with HCN and holistically advance; physically, mentally, emotionally and spiritually, to possess
-            greater awareness and a surplus capacity to effortlessly help others and the progression of the planet. HCN
-            is also a platform that brings together a network of like minded Conscious Creatures (conscious; consumers,
-            producers, creators and communicators) as a collective representation to transform our world. Consume,
-            Contribute, Disperse, and Sponsor media which uplifts the human spirit, empowers people and nurtures the
-            planet.
+            {data.text.siteDescription}
           </p>
           <p className={classes.paragraphTextSmall}>
-            Second Genome, and it's messenger system, represents how we as humans and humanity have the ability to
-            evolve beyond our current state to collectively create Inter-Generational Global Family Wealth. Connect as a
-            Conscious Creature on this site and learn more of what that means!{' '}
+            {data.text.siteDescriptionSecondPara}
             <RouterLink to="#">Connect Me!</RouterLink>
           </p>
         </Grid>
@@ -81,36 +69,33 @@ function SectionDescription(props) {
             <Grid item xs={12} sm={12} md={5}>
               <Grid container direction="column" className={classes.aboutUsContainer}>
                 <Grid item className={`${classes.fontBase} ${classes.aboutUsTitle}`}>
-                  ABOUT US
+                  {data.text.aboutUs}
                 </Grid>
                 <Grid item className={`${classes.fontBase} ${classes.aboutUsText}`}>
-                  HCN and the Sanctuary Project are the ofspring of Vision for Humanity, a New Zealand charitable trust
-                  with aims to create a Global Family and a Successful Planet. Long term, HCN is intended to disperse
-                  content from The Urban Sanctuary's health and wellbeing professionals, guides and workshops.
+                  {data.text.aboutUsContent}
                 </Grid>
               </Grid>
             </Grid>
             <Grid item xs={12} sm={12} md={12}>
               <Grid container direction="column" className={classes.metamorphicMediaContainer}>
                 <Grid item>
-                  <div className={`${classes.fontBase} ${classes.metamorphicMediaTitle}`}>METAMORPHIC MEDIA</div>
+  <div className={`${classes.fontBase} ${classes.metamorphicMediaTitle}`}>{data.text.metamorphicTitle}</div>
                 </Grid>
                 <Grid item>
                   <div className={`${classes.fontBase} ${classes.metamorphicMediaText}`}>
-                    Elevating Global Consciousness through all forms of information and entertainment. Visualise a world
-                    where the media induces mental / emotional well-bring and social cohesion.
+                    {data.text.metamorphicMediaText}
                   </div>
                 </Grid>
                 <Grid item>
                   <div className={`${classes.fontBase} ${classes.metamorphicMediaCaption}`}>
-                    Vision for Media, vision for Humanity
+                    {data.text.metamorphicCaption}
                   </div>
                 </Grid>
                 <Grid item>
                   {/* TODO get link for this button  */}
                   <RouterLink className={classes.linkBase} to="#">
                     <Button className={`${classes.fontBase} ${classes.buttonBase} ${classes.metamorphicMediaButton}`}>
-                      TAKE ME DEEPER
+                      {data.text.metamorphicButton}
                     </Button>
                   </RouterLink>
                 </Grid>
@@ -122,7 +107,7 @@ function SectionDescription(props) {
         <Grid item xs={12} sm={12} md={6}>
           <div className={classes.videoParentContainer}>
             <div className={classes.videoContainer}>
-              <span className={`${classes.fontBase} ${classes.videoContainerTitle}`}>OUR VOLUNTEER BASED HCN TEAM</span>
+  <span className={`${classes.fontBase} ${classes.videoContainerTitle}`}>{data.text.videoTitle}</span>
               <video className={classes.videoVolunteers} controls poster={data.volunteerVideoPoster}>
                 <source src={data.volunteerVideoURL} type="video/mp4" />
                 Your browser does not support HTML5 video.
@@ -135,8 +120,7 @@ function SectionDescription(props) {
               </IconButton>
             </div>
             <div className={`${classes.fontBase} ${classes.videoCaption}`}>
-              HCN is currently being established by a band of dedicated volunteers directed by the VFH founder Kim
-              Larking. Watch this brief video to see out team (video of volunteers and me talking about HCN).
+              {data.text.videoCaption}
             </div>
           </div>
         </Grid>
@@ -161,7 +145,7 @@ function SectionDescription(props) {
               {/* TODO change "to" attrubute to correct link  */}
               <RouterLink className={classes.linkBase} to="#">
                 <Button className={`${classes.fontBase} ${classes.buttonBase} ${classes.urbanSanctuaryButton}`}>
-                  VISIT NOW
+                  {data.text.urbanButton}
                 </Button>
               </RouterLink>
             </Grid>
@@ -175,8 +159,8 @@ function SectionDescription(props) {
         <Grid item xs={12} sm={10} md={6}>
           {/* TODO get correct image */}
           <img className={classes.metawheelImage} src={Metawheel} alt="Metawheel Graphic" />
-          <div className={`${classes.fontBase} ${classes.metawheelTitle}`}>TAKE THE METAWHEEL JOURNEY</div>
-          <div className={`${classes.fontBase} ${classes.metawheelText}`}>Sanctuary Human Advancement Architecture</div>
+  <div className={`${classes.fontBase} ${classes.metawheelTitle}`}>{data.text.metawheelTitle}</div>
+  <div className={`${classes.fontBase} ${classes.metawheelText}`}>{data.text.metawheelText}</div>
         </Grid>
         <Grid item sm={1} md={1} />
       </Grid>
