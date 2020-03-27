b6c75be3caa0d2fd94d95439efe4b266a75f8d29
Dreamufor <missmengxue@gmail.com>
2020-03-27 16:47:00 +1300

    add UI component ArticleBox

---
+++ src/components/ui/OrganizeArticleBox/OrganizeArticleBox.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import {Typography} from '@material-ui/core';
+
+const useStyles = (bgImage) =>
+  makeStyles({
+    content: {
+      backgroundImage: `url(${bgImage})`,
+      backgroundSize: 'auto 100%',
+      backgroundRepeat: 'none',
+      padding: '5%',
+      height: '100%',
+      overflow: 'hidden'
+    },
+    textContent: {
+      float: 'right',
+      width: '35%'
+    },
+    heading: {
+      fontWeight: 'bold',
+      letterSpacing: '3px'
+    }
+  });
+
+function OrganizeArticleBox(props) {
+  const classes = useStyles(props.bgImage)();
+  return (
+    <div className={classes.content}>
+      <div className={classes.textContent}>
+        <Typography className={classes.heading}>Articles</Typography>
+        <Typography>{props.articleContent}</Typography>
+      </div>
+    </div>
+  );
+}
+
+export default OrganizeArticleBox;
