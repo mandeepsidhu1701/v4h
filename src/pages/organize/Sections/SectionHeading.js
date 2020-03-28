504d0dbdc120c8bef99ce8b7a0a9bf1e83520eab
Dreamufor <missmengxue@gmail.com>
2020-03-28 14:42:22 +1300

    updateSectionHeading

--- src/pages/organize/Sections/SectionHeading.js
+++ src/pages/organize/Sections/SectionHeading.js
@@ -1,36 +1,24 @@
 import React from 'react';
-import {withStyles, Typography, Container, Grid} from '@material-ui/core';
+import {withStyles, Typography, Grid} from '@material-ui/core';
 import styles from './SectionHeadingStyle';
 import {Sphere} from '@/components/ui';
-import globeIcon from '@/assets/images/icons/globe-02.png';
-import ssanctuaryIcon from '@/assets/images/icons/sanctuary-03.png';
 
-function SectionHeading({title, description, classes}) {
+function SectionHeading({humanImage, title, description, classes, subHeading}) {
   return (
     <Grid container className={classes.content}>
-      <Grid item md={8} sm={12}>
-        {' '}
-        <Typography className={classes.mainHeading}>{title}</Typography>
-        <div className={classes.linkContainer}>
-          {' '}
-          <div className={classes.oneLink}>
-            <img className={classes.linkIcon} src={globeIcon} />
-            <span>Vitality and Longevity</span>
-          </div>
-          <div className={classes.oneLink}>
-            <img className={classes.linkIcon} src={ssanctuaryIcon} />
-            <span>Bio-interdependence</span>
-          </div>
+      <Grid item md={8} sm={8} xs={12} className={classes.headingPart}>
+        <div className={classes.humanimg}>
+          <img src={humanImage} className={classes.img} />
         </div>
+        <Typography className={classes.mainHeading}>{title}</Typography>
+        <div className={classes.linkContainer}>{subHeading()}</div>
         <Typography className={classes.contentDescription}>{description}</Typography>
       </Grid>
-      <Grid item md={4}>
-        {/* <div style={{float: 'right'}}>
-          <Sphere type={0} sphereRad={60} width={120} height={120} />
-          <Sphere type={1} sphereRad={60} width={120} height={120} />
-          <Sphere type={2} sphereRad={60} width={120} height={120} />
-          <Sphere type={3} sphereRad={60} width={120} height={120} />
-        </div> */}
+      <Grid item md={4} sm={8} xs={12} className={classes.spherePart}>
+        <Sphere type={0} sphereRad={60} width={120} height={120} />
+        <Sphere type={1} sphereRad={60} width={120} height={120} />
+        <Sphere type={2} sphereRad={60} width={120} height={120} />
+        <Sphere type={3} sphereRad={60} width={120} height={120} />
       </Grid>
     </Grid>
   );
